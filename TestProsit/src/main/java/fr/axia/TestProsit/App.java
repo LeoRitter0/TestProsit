0bda60cefdb821eac376a8090918bee0f25f39b5
singe <singe@LAPTOP-15K6OV8B>
2019-05-09 23:01:13 +0200

    Test

---
+++ TestProsit/src/main/java/fr/axia/TestProsit/App.java
@@ -0,0 +1,13 @@
+package fr.axia.TestProsit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
